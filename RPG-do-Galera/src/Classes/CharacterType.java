93929bc3e3b5dae805d26f3c494b4b31579dc405
Marcelo <marcelo_galera@hotmail.com>
2022-05-23 11:50:32 -0300

    Corrigindo bugs e adicionando os métodos fightPaladin e fightMage

--- RPG-do-Galera/src/Classes/CharacterType.java
+++ RPG-do-Galera/src/Classes/CharacterType.java
@@ -42,50 +42,52 @@ public class CharacterType {
 
 	// LEVEL UP
 	public void lvlUpWarrior() {
-		this.defense += 1.5;
-		this.life += 10.0;
-		this.strenght += 1.5;
+		this.defense += 1.0;
+		this.life += 7.0;
+		this.strenght += 1.0;
 		this.level += 1;
 	}
 
 	public void lvlUpPaladin() {
-		this.defense += 1.0;
-		this.life += 8.0;
-		this.strenght += 2.5;
+		this.defense += 0.8;
+		this.life += 6.0;
+		this.strenght += 1.5;
 		this.level += 1;
 	}
 
 	public void lvlUpMage() {
-		this.defense += 0.5;
+		this.defense += 0.7;
 		this.life += 5.0;
-		this.strenght += 4.0;
+		this.strenght += 2.0;
 		this.level += 1;
 	}
 
 	// LEVEL DOWN
 	public void lvlDownWarrior() {
-		this.defense -= 1.5;
-		this.life -= 10.0;
-		this.strenght -= 1.5;
+		this.defense -= 1.0;
+		this.life -= 7.0;
+		this.strenght -= 1.0;
 		this.level -= 1;
 	}
 
 	public void lvlDownPaladin() {
-		this.defense -= 1.0;
-		this.life -= 8.0;
-		this.strenght -= 2.5;
+		this.defense -= 0.8;
+		this.life -= 6.0;
+		this.strenght -= 1.5;
 		this.level -= 1;
 	}
 
 	public void lvlDownMage() {
-		this.defense -= 0.5;
+		this.defense -= 0.7;
 		this.life -= 5.0;
-		this.strenght -= 4.0;
+		this.strenght -= 2.5;
 		this.level -= 1;
 	}
 	
 	public void fightWarrior(String name, Double strenght, Double defense, Double life) {
-		if(life/(this.strenght - defense) >= this.life/(strenght - this.defense)  ) {
+		//System.out.println(life/(this.strenght / defense));
+		//System.out.println(this.life/(strenght / this.defense));
+		if(life/(this.strenght / defense) <= this.life/(strenght / this.defense)) {
 			System.out.println("Awesome! " + this.name + " defeated a " + name + ".");
 			lvlUpWarrior();
 			System.out.println("Status of " + this.name + " after killed " + name + ": Level "  + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
@@ -93,6 +95,46 @@ public class CharacterType {
 		} else if (this.level > 1 ) {
 			System.out.println("That's sad. You died by a " + name + ". Try again!");
 			lvlDownWarrior();
+			System.out.println("Status of " + this.name + " after died by a " + name + ": Level " + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		} else {
+			System.out.println("That's sad. You died by a " + name + ", but already in level 1. Try again!");
+			System.out.println("Status of " + this.name + " after died by a " + name + ": Level "  + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		}
+	}
+	
+	public void fightPaladin(String name, Double strenght, Double defense, Double life) {
+		//System.out.println(life/(this.strenght / defense));
+		//System.out.println(this.life/(strenght / this.defense));
+		if(life/(this.strenght / defense) <= this.life/(strenght / this.defense)) {
+			System.out.println("Awesome! " + this.name + " defeated a " + name + ".");
+			lvlUpPaladin();
+			System.out.println("Status of " + this.name + " after killed " + name + ": Level "  + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		} else if (this.level > 1 ) {
+			System.out.println("That's sad. You died by a " + name + ". Try again!");
+			lvlDownPaladin();
+			System.out.println("Status of " + this.name + " after died by a " + name + ": Level " + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		} else {
+			System.out.println("That's sad. You died by a " + name + ", but already in level 1. Try again!");
+			System.out.println("Status of " + this.name + " after died by a " + name + ": Level "  + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		}
+	}
+	
+	public void fightMage(String name, Double strenght, Double defense, Double life) {
+		//System.out.println(life/(this.strenght / defense));
+		//System.out.println(this.life/(strenght / this.defense));
+		if(life/(this.strenght / defense) <= this.life/(strenght / this.defense)) {
+			System.out.println("Awesome! " + this.name + " defeated a " + name + ".");
+			lvlUpMage();
+			System.out.println("Status of " + this.name + " after killed " + name + ": Level "  + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
+			System.out.println();
+		} else if (this.level > 1 ) {
+			System.out.println("That's sad. You died by a " + name + ". Try again!");
+			lvlDownMage();
 			System.out.println("Status of " + this.name + " after died by a " + name + ": Level " + this.level + ", Strenght: " + this.strenght + ", Defense: " + this.defense + ", Life: " + this.life);		
 			System.out.println();
 		} else {
--- RPG-do-Galera/src/Tests/TestFight.java
+++ RPG-do-Galera/src/Tests/TestFight.java
@@ -7,16 +7,27 @@ public class TestFight {
 	public static void main(String[] args) {
 
 		// CHARACTERS
-		CharacterType warrior = new CharacterType("Aragorn", 4.0, 7.0, 70.0, 1);
-		CharacterType arthas = new CharacterType("Arthas", 5.5, 5.0, 55.0, 1);
-		CharacterType dumbledore = new CharacterType("Dumbledore", 9.0, 3.0, 38.0, 1);
+		                                          // name  , str, def, life, lvl
+		CharacterType warrior = new CharacterType("Aragorn", 2.0, 3.0, 40.0, 1);
+		CharacterType paladin = new CharacterType("Arthas", 2.5, 2.5, 35.0, 1);
+		CharacterType mage = new CharacterType("Dumbledore", 3.3, 2.2, 32.0, 1);
 		
 		
-	
+//                             name  , str, def, life
 		warrior.fightWarrior("Goblin", 2.0, 1.5, 15.0);
 		warrior.fightWarrior("Orc", 3.0, 2.5, 25.0);
 		warrior.fightWarrior("Medusa", 5.5, 2.5, 65.0);
 		warrior.fightWarrior("Dragon", 20.0, 12.0, 150.0);
+		System.out.println("----------------------------------");
+		paladin.fightPaladin("Goblin", 2.0, 1.5, 15.0);
+		paladin.fightPaladin("Orc", 3.0, 2.5, 25.0);
+		paladin.fightPaladin("Medusa", 5.5, 2.5, 65.0);
+		paladin.fightPaladin("Dragon", 20.0, 12.0, 150.0);
+		System.out.println("----------------------------------");
+		mage.fightMage("Goblin", 2.0, 1.5, 15.0);
+		mage.fightMage("Orc", 3.0, 2.5, 25.0);
+		mage.fightMage("Medusa", 5.5, 2.5, 65.0);
+		mage.fightMage("Dragon", 20.0, 12.0, 150.0);
 		
 	}
 
